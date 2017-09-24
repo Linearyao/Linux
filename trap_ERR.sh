3eb63130bf77f4bc1ac4d3b77409c1405734c86d
Linearyao <read707@sina.com>
2017-09-24 15:45:50 +0800

    Create trap_ERR.sh

---
+++ trap_ERR.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#定义捕捉ERR伪信号的函数
+ERRTRAP()
+{
+        echo "[LINE:$1] Error: Command or function exited with status $?"
+}
+
+fun_will_fail()
+{
+        #函数返回非零值表示错误
+        return 1
+}
+
+fun_will_succed()
+{
+        #返回0表示函数执行成功
+        return 0
+}
+
+#命令或者函数产生的每一个错误都将产生一个ERR伪信号
+#这里执行trap命令是希望通过函数ERRTRAP扑捉这个ERR伪信号
+trap 'ERRTRAP $LINENO' ERR 
+
+#模拟一个失败的命令
+command_not_exist 2>/dev/null
+
+#模拟一个成功执行的函数 
+fun_will_succed
+
+#模拟一个失败了的函数
+fun_will_fail
+
+exit 0
