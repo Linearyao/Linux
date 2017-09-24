4a22b4a2ed763590a1aabd66349200ae2abd95e9
Linearyao <read707@sina.com>
2017-09-24 15:45:12 +0800

    Create trap_DEBUG.sh

---
+++ trap_DEBUG.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#每执行一个命令，shell都将发送一个DEBUG伪信号
+trap 'echo "before execute line:$LINENO, a=$a, b=$b, c=$c"' DEBUG
+
+#赋值语句产生DEBUG伪信号
+a=1
+
+#命令[ 产生DEBUG伪信号
+if [ "$a" -eq 1 ] 
+then
+		 #赋值语句产生DEBUG伪信号
+        b=2 
+else
+		 #赋值语句产生DEBUG伪信号
+        b=1 
+fi
+
+#赋值语句产生DEBUG伪信号
+c=3
+
+#命令echo 产生DEBUG伪信号
+echo "end"
