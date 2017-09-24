111a9c99b381da59f5824b55ceb8c601a91651db
Linearyao <read707@sina.com>
2017-09-24 15:44:01 +0800

    Create lack_space.sh

---
+++ lack_space.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ANSWER="yes"
+
+#[是一个命令，所有后面的元素都是它的参数
+if [$ANSWER = "yes" ]
+then
+        echo "The answer is yes, so do operation"
+        echo "...."
+fi
+
+
+echo  " Finished."
